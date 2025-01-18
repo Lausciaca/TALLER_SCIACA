b05fff35351c66892cea18481f27d70ae209cb2a
Lau <laureanosciacaluga@gmail.com>
2025-01-18 09:10:46 -0300

    inicio

---
+++ TALLER_SCIACA/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TALLER_SCIACA project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TALLER_SCIACA.settings')
+
+application = get_asgi_application()
