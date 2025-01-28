7484ea60f068c0b7441d0ebde9243eb8012ccd28
Lau <laureanosciacaluga@gmail.com>
2025-01-28 11:53:57 -0300

    Create View clientes

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ClienteListView
+from .views import ClienteListView, ClienteCreate
 
 cliente_patterns =([
-    path('', ClienteListView.as_view(), name='clientes')
+    path('/', ClienteListView.as_view(), name='clientes'),
+    path('/create/', ClienteCreate.as_view(), name='create')
 ], 'cliente')
--- cliente/views.py
+++ cliente/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Cliente
+from .forms import ClienteForm
 
 # Create your views here.
 class ClienteListView(ListView):
     model = Cliente 
+
+class ClienteCreate(CreateView):
+    model = Cliente
+    form_class = ClienteForm
+    success_url = reverse_lazy('cliente:clientes')
+
