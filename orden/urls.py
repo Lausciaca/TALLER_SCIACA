3f30fe56d5188d72b83063f693a111be5faac204
Lau <laureanosciacaluga@gmail.com>
2025-01-30 15:15:23 -0300

    Añadir detail view y delete view a orden

--- orden/urls.py
+++ orden/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import OrdenCreate, OrdenListView
+from .views import OrdenCreate, OrdenListView, OrdenDetailView, OrdenDeleteView
 
 orden_patterns = ([
     path('', OrdenListView.as_view(), name='ordenes'),
+    path('<int:id>/', OrdenDetailView.as_view(), name='detail'),
+    path('<int:id>/delete', OrdenDeleteView.as_view(), name='delete'),
     path('create/', OrdenCreate.as_view(), name='create'),
 ], 'orden')
--- orden/views.py
+++ orden/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
+from django.views.generic import DeleteView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Orden
 from .forms import OrdenForm
 
@@ -13,6 +15,28 @@ class OrdenCreate(CreateView):
     model = Orden
     form_class = OrdenForm
     success_url = reverse_lazy('orden:ordenes')
+
+    def form_valid(self, form):
+        form.instance.estado = 'particular_uno'
+        return super().form_valid(form)
+    
+
     def form_invalid(self, form):
         print("Errores en el formulario:", form.errors)  # Debug en consola
         return super().form_invalid(form)
+
+class OrdenDetailView(DetailView):
+    model = Orden
+    context_object_name = 'orden'
+
+    def get_object(self):
+        # Modifica para que busque la orden por id
+        return get_object_or_404(Orden, id=self.kwargs['id'])
+    
+
+class OrdenDeleteView(DeleteView):
+    model = Orden
+    success_url = reverse_lazy('orden:ordenes')
+    
+    def get_object(self, queryset=None):
+        return self.model.objects.get(id=self.kwargs['id'])  # Buscar por 'id'
